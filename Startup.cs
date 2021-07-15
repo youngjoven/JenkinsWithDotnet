0e1357c412e27ad916ebe9ff4abdc318188bfb4c
young <>
2021-07-15 17:05:33 +0900

    2nd

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello young 1 ^^ !");
+                    await context.Response.WriteAsync("Hello young 2 ^^ !");
                 });
             });
         }
